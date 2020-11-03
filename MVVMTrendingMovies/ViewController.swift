8680bafa4b3d66fceaeb8c7ca0d84b0dc0f87193
Sashen <sashen.singh@onefootball.com>
2020-11-03 19:08:12 +0100

    initial commit

---
+++ MVVMTrendingMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMTrendingMovies
+//
+//  Created by Sashen Singh on 03.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
