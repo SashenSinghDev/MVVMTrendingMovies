a64f0c9c76309edd40b6dda32dfdfe9d3311259d
Sashen <sashen.singh@onefootball.com>
2020-11-06 13:07:46 +0100

    setting up basic folder structure

---
+++ MVVMTrendingMovies/Features/MovieList/MovieListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListViewController.swift
+//  MVVMTrendingMovies
+//
+//  Created by Sashen Singh on 06.11.20.
+//
+
+import Foundation
+import UIKit
+
+final class MovieListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
